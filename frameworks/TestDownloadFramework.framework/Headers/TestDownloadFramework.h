c952bbf8355b1e8f4cf37c05fb065a91b20390a5
王宏玉 <hongyuwang@pptv.com>
2019-02-01 18:34:00 +0800

    frameword

---
+++ frameworks/TestDownloadFramework.framework/Headers/TestDownloadFramework.h
@@ -0,0 +1,19 @@
+//
+//  TestDownloadFramework.h
+//  TestDownloadFramework
+//
+//  Created by hongyuwang on 2019/2/1.
+//  Copyright © 2019 hongyuwang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TestDownloadFramework.
+FOUNDATION_EXPORT double TestDownloadFrameworkVersionNumber;
+
+//! Project version string for TestDownloadFramework.
+FOUNDATION_EXPORT const unsigned char TestDownloadFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestDownloadFramework/PublicHeader.h>
+
+
